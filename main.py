0c61eaa6abe71bd67fcd4f13ec219a022cb1fb32
IzabellGersh <71597481+IzabellGersh@users.noreply.github.com>
2022-05-24 22:55:33 +0300

    last

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ import dateutil.parser
 app = Flask(__name__)
 
 
-def handle_repo_event(info: json) -> bool:
+def handle_repo_event(data: dict) -> bool:
     req_timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
-    data = json.loads(info)
-    if isinstance(data, dict) and data.get('action') == 'deleted':
+    if data.get('action') == 'deleted':
         repo_data = data.get('repository')
         if isinstance(repo_data, dict):
             created_timestamp = dateutil.parser.parse(repo_data.get('created_at'))
@@ -24,23 +23,20 @@ def handle_repo_event(info: json) -> bool:
     return False
 
 
-def handle_push_event(info: json) -> bool:
-    data = json.loads(info)
-    if isinstance(data, dict):
-        head_commit = data.get('head_commit')
-        if isinstance(head_commit, dict):
-            push_timestamp = head_commit.get('timestamp')
-            push_time = dateutil.parser.parse(push_timestamp)
-            quit_start_time = dateutil.parser.parse('14:00:00')
-            quit_end_time = dateutil.parser.parse('16:00:00')
-            if quit_start_time.time() <= push_time.time() <= quit_end_time.time():
-                return True
+def handle_push_event(data: dict) -> bool:
+    head_commit = data.get('head_commit')
+    if isinstance(head_commit, dict):
+        push_timestamp = head_commit.get('timestamp')
+        push_time = dateutil.parser.parse(push_timestamp)
+        quit_start_time = dateutil.parser.parse('14:00:00')
+        quit_end_time = dateutil.parser.parse('16:00:00')
+        if quit_start_time.time() <= push_time.time() <= quit_end_time.time():
+            return True
     return False
 
 
-def handle_team_event(info: json) -> bool:
-    data = json.loads(info)
-    if isinstance(data, dict) and data.get('action') == 'created':
+def handle_team_event(data: dict) -> bool:
+    if data.get('action') == 'created':
         team = data.get('team')
         if isinstance(team, dict):
             team_name = team.get('name')
@@ -60,18 +56,20 @@ events_dict = {
 def github_webhook():
     if request.headers['Content-Type'] == 'application/json':
         info = json.dumps(request.json)
-        event = request.headers['X-Github-Event']
+        event = request.headers.get('X-Github-Event')
         try:
             handler = events_dict.get(event)
             if handler is None:
                 print(f'please implement handler for the {event}')
                 return
-            is_suspicious = handler(info)
-            if is_suspicious:
-                print(f'Suspicious behavior caused by: {event} event')
+            data = json.loads(info)
+            if isinstance(data, dict):
+                is_suspicious = handler(data)
+                if is_suspicious:
+                    print(f'Suspicious behavior caused by: {event} event')
         except Exception:
             print(f'event:{event} handler crashed')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
